19392cb15ce5b15b06e1fbfcb54654428f049d95
Brian Rheaume-Michaud <brheaume@c2r5p6.42quebec.com>
2023-03-27 15:24:58 -0400

    added client struct

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 11:08:24 by brheaume          #+#    #+#             */
-/*   Updated: 2023/03/20 12:53:34 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/03/27 12:44:07 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,14 @@ typedef struct s_servinfo
 	char	*message;
 	int		cpid;
 }				t_servinfo;
+
+typedef struct s_clinfo
+{
+	char	*message;
+	int		i;
+	int		current_bit;
+	int		spid;
+	int		length;
+}			t_clinfo;
 
 #endif
